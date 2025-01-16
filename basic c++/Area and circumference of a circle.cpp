d0398118f1bd58bb4d6d6139cb99faaa8e4bd698
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2025-01-16 21:27:47 +0530

    Add files via upload

---
+++ basic c++/Area and circumference of a circle.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	float a,c,r;
+	printf("Enter the value of r:-");
+	scanf("%f",&r);
+	c=(2*3.14*r);
+	a=(3.14*r*r);
+	printf("The Value of c%f",c);
+	printf("The Value of a%f",a);
+	return 0;
+}
