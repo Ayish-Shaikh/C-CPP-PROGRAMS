0d326046ca91b7178045dc6ee1c4075db17cab22
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2025-02-05 22:38:55 +0530

    Add files via upload

---
+++ basic c++/odd_even.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int x,even,odd;
+	printf("Enter the value:-");
+	scanf("%d",&x);
+	if (x%2 == 0){
+	printf("even number\n");
+	}
+	else{
+	printf("odd number\n");
+	}
+}
